9fd069461204ec4a2c852e34e462c8acd2dcfa51
Mark T. Holder <mtholder@gmail.com>
2020-04-10 15:27:44 -0500

    more tests

--- peyutil/test/test_str_util.py
+++ peyutil/test/test_str_util.py
@@ -15,6 +15,26 @@ import time
 import os
 
 class TestStrUtil(unittest.TestCase):
+
+    def test_slugify(self):
+        self.assertEqual(slugify("My favorites!"), 'my-favorites')
+        self.assertEqual(slugify(""), 'untitled')
+        self.assertEqual(slugify("Trees about bees"), 'trees-about-bees')
+        self.assertEqual(increment_slug('trees-about-bees'), 'trees-about-bees-2')
+        self.assertEqual(increment_slug('trees-about-bees-2'), 'trees-about-bees-3')
+
+    def test_rev_dict(self):
+        x = {1:2, 3:4}
+        self.assertEqual({2:1, 4:3}, reverse_dict(x))
+        self.assertEqual(1, len(reverse_dict({1:2, 3:2})))
+
+    def test_is_str_type(self):
+        self.assertFalse(is_str_type(3))
+        self.assertFalse(is_str_type(12345678901234567890))
+        self.assertTrue(is_str_type('3'))
+        self.assertFalse(is_str_type(3.2))
+        self.assertTrue(is_str_type(u'διακριτικός'))
+
     def test_any_early_exit(self):
         buf, wrapper = get_utf_8_string_io_writer()
         wrapper.write('test string')
