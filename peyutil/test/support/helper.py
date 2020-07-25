50861180c49c18c992dd4c55ae323fb8390d99df
Mark T. Holder <mtholder@gmail.com>
2020-07-25 15:11:19 -0500

    more docstrings and warning suppression

--- peyutil/test/support/helper.py
+++ peyutil/test/support/helper.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
-from peyutil.str_util import is_str_type
+"""A few utility functions for making tests terser."""
+
 import codecs
 import json
+from peyutil.str_util import is_str_type
 
 
 def testing_read_json(fp):
+    """Reads a UTF-8 JSON from filepath."""
     with codecs.open(fp, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
 def testing_write_json(o, fp):
+    """Writes a UTF-8 JSON to filepath."""
     with codecs.open(fp, 'w', encoding='utf-8') as fo:
         json.dump(o, fo, indent=2, sort_keys=True)
         fo.write('\n')
 
 
 def testing_through_json(d):
+    """Returns a deserialized version of the JSON serialization of `d`."""
     return json.loads(json.dumps(d))
 
 
 def testing_dict_eq(a, b):
-    if a == b:
-        return True
-    return False
+    """You should just call a == b. This is a legacy of a verbose dict compare."""
+    return a == b
 
 
 def testing_conv_key_unicode_literal(d):
+    """Not intended for widespread use. Used in some NexSON tests."""
     r = {}
     if not isinstance(d, dict):
         return d
--- peyutil/test/support/pathmap.py
+++ peyutil/test/support/pathmap.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+"""Path mapping for various test resources."""
 ##############################################################################
 #  Based on DendroPy Phylogenetic Computing Library.
 #
@@ -16,28 +16,28 @@
 #
 ##############################################################################
 
-"""
-Path mapping for various test resources.
-"""
-from peyutil import pretty_timestamp, write_as_json
-
 try:
     import anyjson
 except:
+    # pylint: disable=attribute-defined-outside-init
     import json
 
-
     class Wrapper(object):
+        """No-op class."""
         pass
-
 
     anyjson = Wrapper()
     anyjson.loads = json.loads
 import codecs
 import os
+from peyutil import pretty_timestamp, write_as_json
+
 
 class PathMapForTests(object):
+    """Class with attributes that make it easy to find different types of testing data."""
+
     def __init__(self, path_map_filepath):
+        """Uses the parent directory of `path_map_filepath` to find other testing dirs."""
         support_dir = os.path.dirname(path_map_filepath)
         self.tests_dir = os.path.join(support_dir, os.path.pardir)
         self.package_dir = os.path.join(self.tests_dir, os.path.pardir)
@@ -46,8 +46,8 @@ class PathMapForTests(object):
         self.tests_output_dir = os.path.join(self.tests_dir, "output")
         self.tests_scratch_dir = os.path.join(self.tests_dir, "scratch")
 
-
     def all_files(self, prefix):
+        """Returns a set of filepaths to all test data."""
         d = os.path.join(self.tests_data_dir, prefix)
         s = set()
         for p in os.listdir(d):
@@ -56,48 +56,44 @@ class PathMapForTests(object):
                 s.add(fp)
         return s
 
-
     def nexson_obj(self, filename):
-        """Returns a dict that is the deserialized nexson object
-        'filename' should be the fragment of the filepath below
-        the nexson test dir.
-        """
+        """Returns a JSON load result from `filename` in the TESTS/data/nexson."""
         with self.nexson_file_obj(filename) as fo:
             fc = fo.read()
             return anyjson.loads(fc)
 
-
     def nexson_file_obj(self, filename):
-        """ Returns file object.
-        'filename' should be the fragment of the filepath below
-        the nexson test dir.
-        """
+        """Returns readable file object for testing NexSON in `filename`."""
         fp = self.nexson_source_path(filename=filename)
         return codecs.open(fp, mode='r', encoding='utf-8')
 
-
     def shared_test_dir(self):
+        """Returns the fullpath to the shared-api-tests dir in the tests."""
         return os.path.join(self.tests_data_dir, "shared-api-tests")
 
-
     def nexson_source_path(self, filename=None):
+        """Returns the fullpath for testing a NexSON in `filename`."""
         if filename is None:
             filename = ""
         return os.path.join(self.tests_data_dir, "nexson", filename)
 
-
     def nexml_source_path(self, filename=None):
+        """Returns the fullpath for testing a NeXML in `filename`."""
         if filename is None:
             filename = ""
         return os.path.join(self.tests_data_dir, "nexml", filename)
 
-
     def named_output_stream(self, filename=None, suffix_timestamp=True):
+        """Returns writable file stream of the file from a `named_ouput_path call`."""
         fp = self.named_output_path(filename=filename, suffix_timestamp=suffix_timestamp)
         return open(fp, "w")
 
-
     def named_output_path(self, filename=None, suffix_timestamp=True):
+        """Returns a filepath to filename in the testing output dir.
+
+        If `suffix_timestamp` is True, results of `pretty_timestamp` will
+        be appended to the filename.
+        """
         if filename is None:
             filename = ""
         else:
@@ -109,14 +105,14 @@ class PathMapForTests(object):
             os.makedirs(self.tests_output_dir)
         return os.path.join(self.tests_output_dir, filename)
 
-
     def script_source_path(self, filename=None):
+        """Returns the full path to a `filename` in the package's scripts dir."""
         if filename is None:
             filename = ""
         return os.path.join(self.scripts_dir, filename)
 
-
     def next_unique_scratch_filepath(self, fn):
+        """Returns the full path to a scratch file starting with `fn` in the scratch dir."""
         frag = os.path.join(self.tests_scratch_dir, fn)
         if os.path.exists(self.tests_scratch_dir):
             if not os.path.isdir(self.tests_scratch_dir):
@@ -127,10 +123,8 @@ class PathMapForTests(object):
             os.makedirs(self.tests_scratch_dir)
         return self.next_unique_filepath(frag)
 
-
     def next_unique_filepath(self, fp):
-        """Not thread safe.
-        """
+        """Not thread safe! adds numeric suffix to `fp` until the path does not exist."""
         if os.path.exists(fp):
             ind = 0
             while True:
@@ -140,63 +134,48 @@ class PathMapForTests(object):
                 ind += 1
         return fp
 
-
     def json_source_path(self, filename=None):
+        """Returns the fullpath for testing a JSON in `filename`."""
         if filename is None:
             filename = ""
         return os.path.join(self.tests_data_dir, "json", filename)
 
-
     def collection_obj(self, filename):
-        """Returns a dict that is the deserialized collection object
-        'filename' should be the fragment of the filepath below
-        the collection test dir.
-        """
+        """Returns a JSON load result from `filename` in the collection test dir."""
         with self.collection_file_obj(filename) as fo:
             fc = fo.read()
             return anyjson.loads(fc)
 
-
     def collection_file_obj(self, filename):
-        """ Returns file object.
-        'filename' should be the fragment of the filepath below
-        the collection test dir.
-        """
+        """Returns a readable file object from `collection_source_path` call."""
         fp = self.collection_source_path(filename=filename)
         return codecs.open(fp, mode='r', encoding='utf-8')
 
-
     def collection_source_path(self, filename=None):
+        """Returns a absolute  filepath to `filename` in TESTS/data/colletions dir."""
         if filename is None:
             filename = ""
         return os.path.join(self.tests_data_dir, "collections", filename)
 
-
     def amendment_obj(self, filename):
-        """Returns a dict that is the deserialized amendment object
-        'filename' should be the fragment of the filepath below
-        the amendment test dir.
-        """
+        """Returns a JSON load result from `filename` in the amendments test dir."""
         with self.amendment_file_obj(filename) as fo:
             fc = fo.read()
             return anyjson.loads(fc)
 
-
     def amendment_file_obj(self, filename):
-        """ Returns file object.
-        'filename' should be the fragment of the filepath below
-        the amendment test dir.
-        """
+        """Returns a readable file object from `amendment_source_path` call."""
         fp = self.amendment_source_path(filename=filename)
         return codecs.open(fp, mode='r', encoding='utf-8')
 
-
     def amendment_source_path(self, filename=None):
+        """Returns a absolute  filepath to `filename` in TESTS/data/amendments dir."""
         if filename is None:
             filename = ""
         return os.path.join(self.tests_data_dir, "amendments", filename)
 
     def equal_blob_check(self, unit_test, diff_file_tag, first, second):
+        """Trips unit_test failure if `first` != `second` after writing diff files."""
         if first != second:
             # dd = DictDiff.create(first, second)
             ofn = self.next_unique_scratch_filepath(diff_file_tag + '.obtained_rt')
@@ -209,6 +188,7 @@ class PathMapForTests(object):
                 m = m_fmt.format(o=ofn, e=efn)
                 unit_test.assertEqual("", m)
 
-def get_test_path_mapper():
-    return PathMapForTests(path_map_filepath=__file__)
 
+def get_test_path_mapper():
+    """Factory for PathMapForTests object for the package."""
+    return PathMapForTests(path_map_filepath=__file__)
