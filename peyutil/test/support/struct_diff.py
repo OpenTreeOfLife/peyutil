255cfa73e044d39fe2c34e7681a1b46cc2cc3e90
Mark T. Holder <mtholder@gmail.com>
2020-07-27 09:59:02 -0500

    docs and warnings

--- peyutil/test/support/struct_diff.py
+++ peyutil/test/support/struct_diff.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
+"""Classes for producing more readable diffs between dicts and lists.
+
+Intended to be useful in diagnosing the causes of test failures when
+a returned dict or list is not identical to the expected content.
+"""
 from __future__ import absolute_import, print_function, division
 
 
 class DictDiff(object):
+    """Class for creating readable diffs between dicts.
+
+    Use the DictDiff.create factory function to avoid pitfalls
+    associated with having to learn all of the methods of this class.
+    """
     def __init__(self):
+        """Use the DictDiff.create method."""
         self._additions = []
         self._deletions = []
         self._modifications = []
 
     def finish(self):
+        """Sorts edits. Not needed if you used the DictDiff.create."""
         self._additions.sort()
         self._deletions.sort()
         self._modifications.sort()
 
     def edits_expr(self, par=''):
+        """Should probably be private. Returns a list of strings describing edits."""
         r = self.additions_expr(par=par)
         r.extend(self.deletions_expr(par=par))
         r.extend(self.modification_expr(par=par))
         return r
 
     def additions_expr(self, par=''):
+        """Returns a list of strings describing additions. `par` can be a prefix."""
         r = []
         for k, v in self._additions:
             s = '{p}[{k}] = {v}'.format(p=par, k=repr(k), v=repr(v))
@@ -27,6 +41,7 @@ class DictDiff(object):
         return r
 
     def modification_expr(self, par=''):
+        """Returns a list of strings describing modifications. `par` can be a prefix."""
         r = []
         for k, v in self._modifications:
             pk = '{p}[{k}]'.format(p=par, k=repr(k))
@@ -39,6 +54,7 @@ class DictDiff(object):
         return r
 
     def deletions_expr(self, par=''):
+        """Returns a list of strings describing deletions. `par` can be a prefix."""
         r = []
         for deletion in self._deletions:
             k = deletion[0]
@@ -47,15 +63,19 @@ class DictDiff(object):
         return r
 
     def add_addition(self, k, v):
+        """Records a new k->v pair. Don't call, if you used the factory."""
         self._additions.append((k, v))
 
     def add_deletion(self, k, v):
+        """Records a missing k->v pair. Don't call, if you used the factory."""
         self._deletions.append((k, v))
 
     def add_modification(self, k, v):
+        """Records a new value for k->v pair. Don't call, if you used the factory."""
         self._modifications.append((k, v))
 
     def patch(self, src):
+        """Applies the DictDiff to `src` dict."""
         for k, v in self._deletions:
             del src[k]
         for k, v in self._additions:
@@ -68,7 +88,9 @@ class DictDiff(object):
 
     @staticmethod
     def create(src, dest, **kwargs):
-        """Inefficient comparison of src and dest dicts.
+        """Factory function for a DictDiff object comparing `src` to `dest`.
+
+        Inefficient comparison of src and dest dicts.
         Recurses through dict and lists.
         returns None if there is no difference and a
         DictDiffObject of there are differences
@@ -79,7 +101,7 @@ class DictDiff(object):
                 a list of one dict of the comparison. This
                 is helpful given the BadgerFish convention of
                 emitting single elements as a dict, but >1 elements
-                as a list of dicts:
+                as a list of dicts.
         """
         if src == dest:
             return None
@@ -119,14 +141,23 @@ class DictDiff(object):
 
 
 class ListDiff(object):
+    """Class for creating readable diffs between lists.
+
+    Use the ListDiff.create factory function to avoid pitfalls
+    associated with having to learn all of the methods of this class.
+    """
     def __init__(self):
+        """Use the ListDiff.create method."""
         self._additions = []
         self._modifications = []
         self._deletions = []
 
     @staticmethod
     def create(src, dest, **kwargs):
-        """Inefficient comparison of src and dest dicts.
+        """Factory function for a ListDiff object comparing `src` to `dest`.
+
+        Returns None if the `src` and `dest` are equal.
+        Inefficient comparison of src and dest lists.
         Recurses through dict and lists.
         returns (is_identical, modifications, additions, deletions)
         where each
@@ -137,7 +168,7 @@ class ListDiff(object):
             attributes in dest but not in src
             attributes in src but not in dest
 
-        Returned dicts may alias objects in src, and dest
+        Returned dicts may alias objects in src, and dest.
         """
         if src == dest:
             return None
@@ -187,12 +218,14 @@ class ListDiff(object):
         return diffs
 
     def edits_expr(self, par=''):
+        """Should probably be private. Returns a list of strings describing edits."""
         r = self.modification_expr(par=par)
         r.extend(self.deletions_expr(par=par))
         r.extend(self.additions_expr(par=par))
         return r
 
     def additions_expr(self, par=''):
+        """Returns a list of strings describing additions. `par` can be a prefix."""
         r = []
         for k, ld in self._additions:
             post_del_ind = k[0] + k[1]
@@ -202,6 +235,7 @@ class ListDiff(object):
         return r
 
     def modification_expr(self, par=''):
+        """Returns a list of strings describing modifications. `par` can be a prefix."""
         r = []
         for k, le in self._modifications:
             v = le.obj
@@ -215,6 +249,7 @@ class ListDiff(object):
         return r
 
     def deletions_expr(self, par=''):
+        """Returns a list of strings describing deletions. `par` can be a prefix."""
         # _deletions are reverse sorted
         r = []
         for deletion in self._deletions:
@@ -224,6 +259,7 @@ class ListDiff(object):
         return r
 
     def patch(self, src):
+        """Applies the ListDiff to `src` list."""
         for k, lem in self._modifications:
             v = lem.obj
             if isinstance(v, DictDiff) or isinstance(v, ListDiff):
@@ -239,60 +275,82 @@ class ListDiff(object):
             src.insert(post_del_ind, v)
 
     def add_deletion(self, ind, obj):
+        """Records a missing `obj` at `ind`. Don't call, if you used the factory."""
         tup = (ind, ListDeletion(ind, obj))
         self._deletions.append(tup)
 
     def add_insertion(self, ind, add_offset, obj):
+        """Records a new `obj` at `ind`. Don't call, if you used the factory."""
         sortable_key = (ind, add_offset)
         tup = (sortable_key, ListAddition(ind, add_offset, obj))
         self._additions.append(tup)
 
     def add_modificaton(self, ind, obj):
+        """Records altered `obj` at `ind`. Don't call, if you used the factory."""
         tup = (ind, ListElModification(ind, obj))
         self._modifications.append(tup)
 
     def finish(self):
+        """Sorts edits. Not needed if you used the factory."""
         self._modifications.sort()
         self._deletions.sort(reverse=True)
         self._additions.sort()
 
 
 class ListEdit(object):
+    """Base class for encapsulating a single edit to a list."""
+
     def __init__(self, src_ind, obj):
+        """Notes `obj` at `src_ind` in edited form."""
         self.src_index = src_ind
         self.obj = obj
 
 
 class ListDeletion(ListEdit):
+    """Subclass for encapsulating a deletion to a list."""
+
     def __init__(self, src_ind, obj):
+        """Same as base class init."""
         ListEdit.__init__(self, src_ind, obj)
 
     def __repr__(self):
+        """Standard repr."""
         return 'ListDeletion({s}, {o})'.format(s=self.src_index, o=repr(self.obj))
 
     def __str__(self):
+        """Str calls repr."""
         return repr(self)
 
 
 class ListAddition(ListEdit):
+    """Subclass for encapsulating an addition to a list."""
+
     def __init__(self, src_ind, add_offset, obj):
+        """Same as base class init."""
         ListEdit.__init__(self, src_ind, obj)
         self.add_offset = add_offset
 
     def __repr__(self):
+        """Standard repr."""
         return 'ListAddition({s}, {o})'.format(s=self.src_index, o=repr(self.obj))
 
     def __str__(self):
+        """Str calls repr."""
         return repr(self)
 
 
 class ListElModification(ListEdit):
+    """Subclass for encapsulating a modification to a list."""
+
     def __init__(self, src_ind, obj):
+        """Same as base class init."""
         ListEdit.__init__(self, src_ind, obj)
 
     def __repr__(self):
+        """Standard repr."""
         return 'ListElModification({s}, {o})'.format(s=self.src_index,
                                                      o=repr(self.obj))
 
     def __str__(self):
+        """Str calls repr."""
         return repr(self)
