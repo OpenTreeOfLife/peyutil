d1449ca4e33371371acb3b294390b1b7dd11e85c
Mark T. Holder <mtholder@gmail.com>
2025-04-15 18:00:28 -0500

    working on first flit build

--- src/peyutil/__init__.py
+++ src/peyutil/__init__.py
@@ -7,7 +7,7 @@ do not depend on any part of peyotl.
 """
 from __future__ import absolute_import, print_function, division
 
-__version__ = "0.0.5"  # sync with setup.py
+__version__ = "0.0.6"
 
 import time
 import os
